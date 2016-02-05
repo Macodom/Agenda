93d478a1fcec619461cbc7d1138369d1b51d105b
Macodom <macodom@gmail.com>
2016-02-05 16:34:20 -0600

    Se configuro correctamente el ordenamiento de las carpetas

--- App/Cliente/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-
-class Client(models.Model):
-    name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50)
-
-    def __unicode__(self):
-        return "%s" % (self.nombre)
