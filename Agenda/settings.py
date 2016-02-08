afb19b579f05572c1a4bd3cd46eba1637d999078
Macodom <macodom@gmail.com>
2016-02-08 16:10:51 -0600

    Agregado admin Django

--- Agenda/settings.py
+++ Agenda/settings.py
@@ -129,3 +129,12 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+TEMPLATES_DIRS = [os.path.join(BASE_DIR, 'templates')]
+
+#STATICFILES_DIRS = os.path.join(BASE_DIR, 'static')
+
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'images')
+
