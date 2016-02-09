a6d90f38ef5f617ecdd3871e5dcff1e0061afc22
Macodom <macodom@gmail.com>
2016-02-09 17:21:34 -0600

    El template adminis tiene el template mas limpio, usarlo para base de los demas template

--- Agenda/App/Recepcion/views.py
+++ Agenda/App/Recepcion/views.py
@@ -4,7 +4,7 @@ from django.shortcuts import render
 from .models import Reception
 
 def login(request):
-    return render_to_response('formulario_usuario.html', context_instance=RequestContext(request))
+    return render_to_response('index_recepcion.html', context_instance=RequestContext(request))
 
 # def index(request):
 #     return render_to_response('index.html', context_instance=RequestContext(request))
